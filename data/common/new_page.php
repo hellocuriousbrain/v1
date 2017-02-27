7a3ecfdcdcaae7cf2eb1e2796df1955d4a909143
Arun <imarunsinghal@gmail.com>
2017-02-27 14:42:29 +0530

    Changes for Bugs-4

---
+++ data/common/new_page.php
@@ -0,0 +1,82 @@
+<?php
+function new_page($currpageId) {
+   ?>
+
+<html>
+<?php include 'head.php' ?>
+<body>
+<?php include 'side_feedback.php'; ?>
+
+<center>
+<table >
+<tbody>
+<!-- Full Start (This Contain Two tables (Right and Left)) -->
+	<tr>
+		<td class="left" valign="top">
+    		<!-- Left Table Start -->     
+		<table class="left_container" align="left" border="0" cellpadding="0" cellspacing="0">
+			<!--First Row of Left Table start -->
+			<tr>
+				<td valign="top">
+				<?php include 'logo.php'; ?>
+				</td>
+			</tr>
+			<!--First Row of Left Table End -->
+
+			<!--Second Row of Left Table Start -->
+			<tr>
+				<td width="250px" valign="top" style="height:377px;" >
+				<?php 
+					include  "left_".$currpageId."_adv.php"; 
+				?>
+				</td>
+			</tr>
+ 
+			<tr>
+				<td width="250px" valign="top" style="height:377px;" >
+				<?php include "quick_links_".$currpageId.".php"; ?>
+				</td>
+			</tr>
+       
+			<!--Second Row of Left Table Ends -->
+		</table>
+		</td>
+		<!-- Left Table End -->
+
+		
+		<td class="right" valign="top">
+		<!-- Right Table Start -->
+		<table class="right_container" align="right" border="0" cellpadding="0"
+		    cellspacing="0" >
+
+			<?php
+				include 'main_menu.php';
+				include 'description.php';
+				descrip($currpageId); 
+			?>
+		</table>
+		</td>
+	</tr>
+
+<tr>
+<td class="left">
+<div>
+<?php include 'footer_left.php'; ?>
+</div>
+<!-- Full Table End -->
+</td>
+<td class="right">
+<?php include 'footer_right.php'; ?>
+</td>
+</tr>
+
+
+  </tbody>
+</table>
+
+</center>
+</body>
+</html>
+
+
+<?php } ?>
