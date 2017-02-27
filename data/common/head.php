3485c0479e9d926f4c21c8a7ab29b36a55aad9fd
Arun <imarunsinghal@gmail.com>
2017-02-27 16:35:59 +0530

    Changes for Bugs-10

--- data/common/head.php
+++ data/common/head.php
@@ -5,7 +5,7 @@
     <script type="text/javascript" src="../data/js/jquery-1.3.1.min.js"></script>
     <script type="text/javascript" src="../data/js/jquery.dropdownPlain.js"></script>
 <title>Curious Brain - Born to Learn</title>
-<link rel="shortcut icon" href="../data/image/icon.png" type="data/image/x-icon" />
+<link rel="shortcut icon" href="../data/image/icon.jpeg" type="data/image/x-icon" />
  <link href="../data/css/js-image-slider.css" rel="stylesheet" type="text/css" />
     <script src="data/js/js-image-slider.js" type="text/javascript"></script>
     <link href="../data/css/generic.css" rel="stylesheet" type="text/css" />
